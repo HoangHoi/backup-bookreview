d67d2722b7b387ca32b203739eb87d4f18a05c16
Hoang Hoi <hoanghoi1310@gmail.com>
2016-10-06 11:00:00 +0700

    abc

--- public/js/adminUser.js
+++ public/js/adminUser.js
@@ -217,6 +217,9 @@ function user() {
             current.request = form.serializeObject();
             inputs.prop('disabled', true);
             current.sendRequest(current.link, function (data) {
+                $.each(data, function (i, v){
+                    console.log(i+':'+v);
+                });
                 current.table.ajax.reload(null, false);
                 if (data.status === 200) {
                     $('#myModal').modal('hide');
@@ -257,6 +260,9 @@ function user() {
             async: false,
             data: current.request,
             complete: function (data) {
+                $.each(data, function (i, v){
+                    console.log(i+':'+v);
+                });
                 if (typeof calback === 'function') {
                     calback(data);
                 }
@@ -288,9 +294,6 @@ function user() {
         var r = confirm(current.lang.trans.confirm_reset_password);
         if (r) {
             this.sendRequest(this.url.reset_password, function (data) {
-                $.each(data, function (i, v){
-                    console.log(i+':'+v);
-                });
                 current.table.ajax.reload(null, false);
                 if (data.status === 200) {
                     message(
